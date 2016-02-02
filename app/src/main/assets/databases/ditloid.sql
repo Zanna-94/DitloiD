3aae9919aa85d1693e1749e548c87a608ff4c6d9
simonemattogno <mellon>
2016-02-02 13:47:04 +0100

    add drawable and string

---
+++ app/src/main/assets/databases/ditloid.sql
@@ -0,0 +1,63 @@
+--
+-- File generated with SQLiteStudio v3.0.7 on lun feb 1 16:31:14 2016
+--
+-- Text encoding used: UTF-8
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: ditloid
+CREATE TABLE ditloid (Enigma TEXT, Soluzione TEXT, Categoria TEXT, Difficoltà INTEGER, Indizio TEXT);
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('365 G in un A', '365 giorni in un anno', 'Cultura Generale', 1, '366 se bisestile');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('180 G in un A P', '180 gradi in un angolo piatto', 'Matematica', 1, '90 se retto');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('26 L nell’A I', '26 lettere nell’alfabeto inglese', 'Cultura Generale', 1, 'A B C D..');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('24000 B', '24000 baci', 'Musica', 1, 'Celentano a Sanremo ‘61');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('90: la P', '90: la paura', 'Proverbi & Detti Popolari', 1, 'Fa 90');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('6 F di un D', '6 facce di un dado', 'Logica', 1, 'La somma delle opposte è sempre 7');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('12 S dello Z', '12 segni dello zodiaco', 'Cultura Generale', 1, 'Sono costellazioni');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('100 C nella D C', '100 canti nella divina commedia', 'Libri & Letteratura', 1, 'Dante Alighieri');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('3 C sul C per la F del D', '3 civette sul comò per la figlia del dottore', 'Proverbi & Detti Popolari', 1, 'Il dottore si ammalò');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('100 C in un E', '100 centesimi in un euro', 'Cultura Generale', 1, 'Le lire erano 1000');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('3 F del S degli A', '3 film del signore degli anelli', 'Film', 2, 'La trilogia che vinse 17 oscar');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('2 F di G', '2 facce di giano', 'Storia', 2, 'Famosa divinità romana');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('0 A in un C', '0 angoli in un cerchio', 'Matematica', 2, 'Poligono regolare con infiniti lati');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('1 O di un C', '1 occhio di un ciclope', 'Libri & Letteratura', 2, 'Ulisse');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('4850 C in C', '4850 cose in comune', 'Musica', 2, 'Daniele Silvestri');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('1 A in un P', '1 ago in un pagliaio', 'Proverbi & Detti Popolari', 2, 'Difficile da trovare');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('40 C in un M N', '40 carte in un mazzo napoletano', 'Cultura Generale', 2, 'Per giocare a briscola');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('33 T E a T T e T T', '33 trentini entrarono a trento tutti e trentatré trotterellando', 'Proverbi & Detti Popolari', 2, 'Scioglilingua');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('21 T al G d’I', '21 tappe al giro d’italia', 'Sport', 2, 'Ciclismo Rosa');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('3 T C T T', '3 tigri contro tre tigri', 'Proverbi & Detti Popolari', 2, 'Scioglilingua');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('3 M S il C', '3 metri sopra il cielo', 'Film / Libri & Letteratura', 2, 'Moccia');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('7 P C', '7 peccati capitali', 'Religione', 2, 'Magnum');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('8 P del S S', '8 pianeti del sistema solare', 'Scienza', 2, 'Plutone non lo è');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('4 C di H', '4 case di hogwarts', 'Libri & Letteratura', 2, '“Non serpeverde, non serpeverde"');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('3 L in un O', '3 lancette in un orologio', 'Cultura Generale', 3, 'Secondi, minuti e ore!');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('100 N nel D', '100 novelle nel decameron', 'Libri & Letteratura', 3, 'Boccaccio');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('1 A nella B (V N)', '1 ago nella bussola (verso nord)', 'Cultura Generale', 3, 'Campo magnetico terrestre');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('9 G dell’I', '9 giorni dell’inferno', 'Libri & Letteratura', 3, 'Divina Commedia');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('10 C sul M S per M', '10 comandamenti sul monte sinai per mosè', 'Religione', 3, 'Tavole della legge');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('3 C del N', '3 cascate del niagara', 'Geografia', 3, 'Al confine tra USA e Canada');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('21 P su un D', '21 puntini su un dado', 'Logica', 3, '1+2+3+4+5+6 = 21');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('3 C di L', '3 cime di lavaredo', 'Geografia', 3, 'Dolomiti');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('1281 G S da P', '1281 gol segnati da pelè', 'Sport', 3, 'Il più grande goleador');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('5 N: V per V', '5 novembre: v per vendetta', 'Film / Libri & Letteratura', 3, 'Ricorda per sempre');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('31 O: D o S?', '31 ottobre: dolcetto o scherzetto?', 'Festività', 3, 'Halloween');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('3 C di K', '3 critiche di kant', 'Letteratura', 3, 'Filosofia');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('7 G in una S di P (con il L)', '7 giocatori in una squadra di pallavolo (con il libero)', 'Sport', 3, 'Pallavolo');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('2 P M (N, S)', '2 poli magnetici (nord, sud)', 'Scienza', 3, 'Campi');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('11 P O per T', '11 premi oscar per titanic', 'Film', 3, 'Record storici');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('7 S del D', '7 sfere del drago', 'Cartoni animati', 3, 'Shenron');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('78 G nella G di M', '78 giri nella gara di montecarlo', 'Sport', 4, 'Formula 1');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('4 T E di un T', '4 triangoli equilateri in un tetraedro', 'Matematica', 4, 'Geometria solida');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('9 A del P per gli U', '9 anelli del potere per gli uomini', 'Film / Libri & Letteratura', 4, 'Nazgul');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('13 A di U', '13 anelli di uranio', 'Scienza', 4, 'Non proprio come Saturno');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('103 E nella T P', '103 elementi nella tavola periodica', 'Scienza', 4, 'Mendeleev');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('67 S di G', '67 satelliti di giove', 'Scienza', 4, 'Astronomia');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('8 O O per M P', '8 ori olimpici per michael phelps', 'Sport', 4, 'Olimpiadi 2008');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('0 R per F', '0 rime per fegato', 'Giochi di Parole', 5, 'Anche “uva” ne ha 0');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('4 S nella R del P P', '4 spine nella rosa del piccolo principe', 'Libri & Letteratura', 5, 'Antoine de Saint-Exupéry');
+INSERT INTO ditloid (Enigma, Soluzione, Categoria, Difficoltà, Indizio) VALUES ('6 A per R', '6 avvoltoi per remo', 'Storia', 5, 'Presagi Romani');
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
