4d5a904b1677871447c497ad5e78ff146689be51
TizianoMenichelli <Onaizit7>
2016-02-04 12:51:22 +0100

    Manca un file

---
+++ app/src/main/java/giava/menmath/ditloid/MyApplication.java
@@ -0,0 +1,22 @@
+package giava.menmath.ditloid;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by tizianomenichelli on 04/02/16.
+ */
+
+public class MyApplication extends Application {
+
+    private static Context context;
+
+    public void onCreate() {
+        super.onCreate();
+        MyApplication.context = getApplicationContext();
+    }
+
+    public static Context getAppContext() {
+        return MyApplication.context;
+    }
+}
