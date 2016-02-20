ea16bb86f918e606734966a798439bb4b1253095
Zanna-94 <emanuele.vannacci@gmail.com>
2016-02-20 19:47:38 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/java/giava/menmath/ditloid/Game/ArrayListFragment.java

--- app/src/main/java/giava/menmath/ditloid/Main.java
+++ app/src/main/java/giava/menmath/ditloid/Main.java
@@ -22,17 +22,7 @@ import giava.menmath.ditloid.User.UserInfo;
 
 public class Main extends AppCompatActivity {
 
-    private Button btnPlay, btnChallenge, btnRules, btnHelpUs;
-    private TextView tvCredits;
-
-    /**
-     * Int value that provides request code for Bluetooth activation intent
-     */
-    private static final int REQUEST_ENABLE_BT = 1;
-
-    private BluetoothAdapter mBluetoothAdapter;
-    private Menu mMenu;
-
+    private Button btnPlay, btnChallenge, btnRules;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,53 +31,11 @@ public class Main extends AppCompatActivity {
         btnPlay = (Button) findViewById(R.id.btnPlay);
         btnChallenge = (Button) findViewById(R.id.btnChallenge);
         btnRules = (Button) findViewById(R.id.btnRules);
-        btnHelpUs = (Button) findViewById(R.id.btnHelpUs);
-
         MyClick mc = new MyClick();
 
         btnPlay.setOnClickListener(mc);
         btnChallenge.setOnClickListener(mc);
         btnRules.setOnClickListener(mc);
-        btnHelpUs.setOnClickListener(mc);
-
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        this.mMenu = menu;
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.action_bar, menu);
-
-        MenuItem myMenuItem = mMenu.findItem(R.id.tvCredits);
-        myMenuItem.setTitle(UserInfo.getInstance().getCredit().toString());
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        //int id = item.getItemId();
-        //if (id == R.id.action_settings) {
-        //return true;
-        //}
-        //return super.onOptionsItemSelected(item);
-
-        switch (item.getItemId()) {
-            case R.id.action_favorite:
-                Toast.makeText(getBaseContext(), "Numero di crediti rimanenti: " + UserInfo.getInstance().getCredit(), Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.action_settings:
-                Toast.makeText(getBaseContext(), "Clicca per le impostazioni", Toast.LENGTH_LONG).show();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     public class MyClick implements View.OnClickListener {
@@ -102,20 +50,13 @@ public class Main extends AppCompatActivity {
 
                 Intent challenge = new Intent(Main.this, BluetoothChallenge.class);
                 startActivity(challenge);
-
             }
             if (v.getId() == R.id.btnRules) {
                 Intent readRules = new Intent(Main.this, Rules.class);
                 startActivity(readRules);
             }
-            if (v.getId() == R.id.btnHelpUs) {
-                Intent Help = new Intent(Main.this, HelpUs.class);
-                startActivity(Help);
-            }
         }
     }
-
-
 }
 
 
--- app/src/main/java/giava/menmath/ditloid/Rules.java
+++ app/src/main/java/giava/menmath/ditloid/Rules.java
@@ -20,33 +20,4 @@ public class Rules extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
     }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.action_bar, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        //int id = item.getItemId();
-        //if (id == R.id.action_settings) {
-        //return true;
-        //}
-        //return super.onOptionsItemSelected(item);
-
-        switch (item.getItemId()) {
-            case R.id.action_favorite:
-                Toast.makeText(getBaseContext(), "Numero di crediti rimanenti: " + UserInfo.getInstance().getCredit(), Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.action_settings:
-                Toast.makeText(getBaseContext(), "Clicca per le impostazioni", Toast.LENGTH_LONG).show();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
