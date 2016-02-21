baca72c19cecc015bd81416805d339b80a8ad2de
Zanna-94 <emanuele.vannacci@gmail.com>
2016-02-21 15:53:48 +0100

    aggiunte regole

--- app/src/main/java/giava/menmath/ditloid/Rules.java
+++ app/src/main/java/giava/menmath/ditloid/Rules.java
@@ -4,10 +4,20 @@ import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Locale;
 
 import giava.menmath.ditloid.User.UserInfo;
 
@@ -16,8 +26,47 @@ import giava.menmath.ditloid.User.UserInfo;
  */
 
 public class Rules extends AppCompatActivity {
+
+
+    private TextView tvRules;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+
+        tvRules = (TextView) findViewById(R.id.tvRules);
+        tvRules.setMovementMethod(new ScrollingMovementMethod());
+
+        String language = Locale.getDefault().getLanguage();
+        StringBuilder text = new StringBuilder();
+        BufferedReader reader;
+
+        InputStream is;
+
+        try {
+
+            if (language.equals("it"))
+                is = getAssets().open("regole.txt");
+            else
+                is = getAssets().open("rules.txt");
+
+            reader = new BufferedReader(new InputStreamReader(is));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            finish();
+        }
+
+
+        tvRules.setText(text.toString());
+
     }
+
 }
